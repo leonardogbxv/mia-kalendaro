25a0dc9d1fde2b4b352b9b6b539cec6635956c5d
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-06-19 13:49:28 -0300

    Fix theme selection problem

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,13 +1,16 @@
 const selectTheme = document.querySelector('#themes select')
 
+const href = document.querySelector('link[href="/styles/main.css"]')
+
 selectTheme.onchange = function() {
   const theme = selectTheme.value
 
-  const href = document.querySelector('link[href="/styles/main.css"]')
-  
   switch (theme) {
     case 'Amrilato':
       href.setAttribute('href','/styles/amrilato.css')
+      break
+    case 'Dark':
+      href.setAttribute('href','/styles/dark.css')
       break
     case 'Default':
       href.setAttribute('href','/styles/main.css')
