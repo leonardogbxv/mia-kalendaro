4ffb900c15e40c02aefc85397af1ff75c3f6278b
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-10-23 16:24:39 -0300

    Fix href file links and refactor js code w/ commas

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,19 +1,19 @@
-const selectTheme = document.querySelector('#themes select')
+const selectTheme = document.querySelector('#themes select');
 
-const href = document.querySelector('link[href="/styles/main.css"]')
+const href = document.querySelector('link[href="styles/main.css"]');
 
 selectTheme.onchange = function() {
-  const theme = selectTheme.value
+  const theme = selectTheme.value;
 
   switch (theme) {
     case 'Amrilato':
-      href.setAttribute('href','/styles/amrilato.css')
-      break
+      href.setAttribute('href','styles/amrilato.css');
+      break;
     case 'Dark':
-      href.setAttribute('href','/styles/dark.css')
-      break
+      href.setAttribute('href','styles/dark.css');
+      break;
     case 'Default':
-      href.setAttribute('href','/styles/main.css')
-      break
+      href.setAttribute('href','styles/main.css');
+      break;
   }
 }
